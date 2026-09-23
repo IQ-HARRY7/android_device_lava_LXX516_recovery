e4d2d203acd94d625d321dbbee124e166653c97d
IQ-HARRY7 <sahubalarama739@gmail.com>
2026-09-23 21:18:50 +0530

    LXX516: The beginning of LXX516
    -@IQ_HARRY_07
    
    Signed-off-by: IQ-HARRY7 <sahubalarama739@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_LXX516.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_LXX516-user \
+    omni_LXX516-userdebug \
+    omni_LXX516-eng
---
+++ README.md
@@ -0,0 +1,11 @@
+# Android device tree for LAVA LAVA LXX516 (LXX516)
+
+```
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
+# Thanks to @IQ_HARRY_07 for the base Tree. 
